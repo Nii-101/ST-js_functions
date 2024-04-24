8db103aa317a5fc71a1350136a7e276857372d4b
Nii-101 <noelamptey101@gmail.com>
2024-04-24 23:57:13 +0000

    Task 3 completed

---
+++ task3.js
@@ -0,0 +1,16 @@
+//Create a function that checks if a number is even or odd and returns "even" or "odd" accordingly. Then, rewrite it using an arrow function.
+
+// Main Function
+function checkIfEvenOrOdd(num){
+    num % 2 === 0 ? console.log('even') : console.log('odd');
+}
+const checkIfEvenOrOddArrowFunc= (num) => {
+    num % 2 === 0 ? console.log('even') : console.log('odd');
+}
+
+
+// TestCases Output: [odd, even, even, odd]
+checkIfEvenOrOdd(25)
+checkIfEvenOrOdd(100)
+checkIfEvenOrOddArrowFunc(30)
+checkIfEvenOrOddArrowFunc(1)
